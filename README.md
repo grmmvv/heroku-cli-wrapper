4173bc45bfe1745f5041657db4cdc6d17603644c
Max Gribennikov <maxim.gribennikov@quantumsoft.ru>
2020-01-30 04:34:02 +0700

    Updated

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # Python wrapper for Heroku CLI
 
-This python module contains wrapper that allows to automatically install last version of [Allure 2](https://github.com/allure-framework/allure2) framework and make reports.
+Python wrapper around, with automatic system-wide (!) installation [Heroku CLI](https://devcenter.heroku.com/articles/heroku-cli).
+
+### Important
+
+This module makes system-wide installation of Heroky CLI!
+
+You need to setup `HEROKU_API_TOKEN` environment variable before you can start using this!
 
 ### Installation
 With pip:
 
-`$ pip install git+https://github.com/grmmvv/heroku-cli-wrapper.git
+`$ pip install git+https://github.com/grmmvv/heroku-cli-wrapper.git`
 
 Or with [pipenv](https://pipenv.kennethreitz.org/en/latest/):
 
@@ -13,13 +19,29 @@ Or with [pipenv](https://pipenv.kennethreitz.org/en/latest/):
 
 ### Usage
 
+In case when heroku app is not exist:
+
 ```python
 
 from heroku_cli_wrapper import HerokuCLIWrapper
 
 if __name__ == '__main__':
+    # If Heroku CLI is not installed then class initialization will install last version
     heroku_client = HerokuCLIWrapper()
-    heroku_client.auth_with_token('HEROKU_API_TOKEN')
-    heroku_client.create_app('my-beautiful-app')
+    heroku_client.create_app()
+    heroku_client.restart_app()
+
+```
+
+In case when need to work with existing app
+
+```python
+
+from heroku_cli_wrapper import HerokuCLIWrapper
+
+if __name__ == '__main__':
+    # If Heroku CLI is not installed then class initialization will install last version
+    heroku_client = HerokuCLIWrapper('my-beautiful-heroku-app')
+    heroku_client.restart_app()
 
 ```
--- src/heroku.py
+++ src/heroku.py
@@ -1,33 +1,43 @@
 import json
+import logging
 import os
-import subprocess
+
+from src.helper import call_cmd
 
 
-def call_cmd(cmd: str):
-    response = subprocess.run(cmd, shell=True, capture_output=True, universal_newlines=True)
-    if response.returncode != 0:
-        print(f'Command "{cmd}" returned non-zero exit status {response.returncode}.')
-        print(response.stderr)
-        exit(response.returncode)
-    return response
+logging.basicConfig(format='%(message)s', level=logging.INFO)
 
 
 class HerokuCLIWrapper:
-    def __init__(self):
+    def __init__(self, app_name: str = None):
+        try:
+            os.environ['HEROKU_API_KEY']
+        except KeyError:
+            logging.error('Unauthorized: unable to find "HEROKU_API_KEY" environment variable')
+            logging.error('Please make sure that you setup "HEROKU_API_KEY" before use')
+            exit(1)
         try:
             call_cmd('heroku --version')
         except SystemExit:
             self._install__heroku_cli()
+        self.app_name = app_name
+        try:
+            self.get_info()
+        except SystemExit:
+            logging.error(f'Unable to find app with name: "{self.app_name}"')
+            exit(1)
 
-    def auth_with_token(self, token: str):
-        os.environ['HEROKU_API_KEY'] = token
-
-    def _install__heroku_cli(self):
-        print('Install Heroku CLI')
+    @staticmethod
+    def _install__heroku_cli():
+        logging.info('Install Heroku CLI')
         call_cmd('curl https://cli-assets.heroku.com/install.sh | sh')
 
+    def get_info(self):
+        cmd = f'heroku apps:info -a {self.app_name} --json'
+        return json.loads(call_cmd(cmd).stdout)
+
     def create_app(self, app_name: str = None, team_name: str = None):
-        print('Create new heroku app')
+        logging.info('Create new heroku app')
         cmd = 'heroku apps:create --json'
         if app_name:
             cmd = f'{cmd} {app_name}'
@@ -35,28 +45,30 @@ class HerokuCLIWrapper:
             cmd = f'{cmd} -t {team_name}'
         r = call_cmd(cmd)
         data = json.loads(r.stdout)
+        self.app_name = data['name']
         return data
 
-    def delete_app(self, app_name: str):
-        cmd = f'heroku apps:destroy -a {app_name} -c {app_name}'
+    def delete_app(self):
+        logging.info(f'Destroy heroku app: "{self.app_name}"')
+        cmd = f'heroku apps:destroy -a {self.app_name} -c {self.app_name}'
         return call_cmd(cmd)
 
-    def get_addons(self, app_name: str):
-        print(f'Get addons list from app: {app_name}')
-        cmd = f'heroku addons -a {app_name} --json'
+    def get_addons(self):
+        logging.info(f'Get addons list from app: {self.app_name}')
+        cmd = f'heroku addons -a {self.app_name} --json'
         r = call_cmd(cmd)
         data = json.loads(r.stdout)
         return data
 
-    def create_addon(self, app_name, addon):
-        print(f'Create addon: {addon} in app: {app_name}')
-        cmd = f'heroku addons:create {addon} -a {app_name} --wait --json'
+    def create_addon(self, addon):
+        logging.info(f'Create addon: {addon} in app: {self.app_name}')
+        cmd = f'heroku addons:create {addon} -a {self.app_name} --wait --json'
         r = call_cmd(cmd)
         return r
 
-    def restore_backup_from_url(self, app_name: str, source_url: str):
-        print(f'Restore DB backup from: {source_url} to app {app_name}')
-        cmd = f'heroku pg:backups:restore {source_url} DATABASE_URL -a {app_name} --confirm {app_name}'
+    def restore_backup_from_url(self, source_url: str):
+        logging.info(f'Restore DB backup from: {source_url} to app {self.app_name}')
+        cmd = f'heroku pg:backups:restore {source_url} DATABASE_URL -a {self.app_name} --confirm {self.app_name}'
         max_attempts = 5
         attempt = 0
         while attempt <= max_attempts:
@@ -64,54 +76,53 @@ class HerokuCLIWrapper:
             if r == 0:
                 break
             else:
-                print(f'Command "{cmd}" returned non-zero exit status {r}.')
+                logging.error(f'Command "{cmd}" returned non-zero exit status {r}.')
                 attempt += 1
         else:
-            print(f'Unable to restore reference database to created app in {max_attempts} attempts')
+            logging.error(f'Unable to restore reference database to created app in {max_attempts} attempts')
             exit(1)
 
-    def copy_database(self, src_db_name: str, dst_app_name: str, dst_db_name: str, src_app_name: str = None):
-        cmd = f'heroku pg:copy {src_db_name} {dst_db_name} --app {dst_app_name} --confirm {dst_app_name}'
+    def copy_database(self, src_db_name: str, dst_db_name: str, src_app_name: str = None):
+        cmd = f'heroku pg:copy {src_db_name} {dst_db_name} --app {self.app_name} --confirm {self.app_name}'
         if src_app_name:
-            cmd = f'heroku pg:copy {src_app_name}::{src_db_name} {dst_db_name} --app {dst_app_name} --confirm {dst_app_name}'
+            cmd = f'heroku pg:copy {src_app_name}::{src_db_name} {dst_db_name} --app {self.app_name} --confirm {self.app_name}'
         call_cmd(cmd)
 
-
-    def get_env_vars(self, app_name: str):
-        print(f'Get environment variables from app: {app_name}')
-        cmd = f'heroku config -a {app_name} --json'
+    def get_env_vars(self):
+        logging.info(f'Get environment variables from app: {self.app_name}')
+        cmd = f'heroku config -a {self.app_name} --json'
         r = call_cmd(cmd)
         data = json.loads(r.stdout)
         return data
 
-    def set_env_vars(self, app_name: str, env_vars_dict: dict):
-        print(f'Set environment variables for app: {app_name}')
+    def set_env_vars(self, env_vars_dict: dict):
+        logging.info(f'Set environment variables for app: {self.app_name}')
         addons_data = ''
         for k, v in env_vars_dict.items():
             addons_data += f'{k}="{v}" '
-        cmd = f'heroku config:set -a {app_name} {addons_data}'
+        cmd = f'heroku config:set -a {self.app_name} {addons_data}'
         r = call_cmd(cmd)
         return r
 
-    def set_remote(self, app_name: str):
-        print(f'Add heroku hemote for app: {app_name}')
-        cmd = f'heroku git:remote -a {app_name}'
+    def set_remote(self):
+        logging.info(f'Add heroku hemote for app: {self.app_name}')
+        cmd = f'heroku git:remote -a {self.app_name}'
         r = call_cmd(cmd)
         return r
 
-    def add_buildpack(self, app_name: str, buildpack_url: str):
-        print(f'Add buildpack: {buildpack_url} to app: {app_name}')
-        cmd = f'heroku buildpacks:add {buildpack_url} -a {app_name}'
+    def add_buildpack(self, buildpack_url: str):
+        logging.info(f'Add buildpack: {buildpack_url} to app: {self.app_name}')
+        cmd = f'heroku buildpacks:add {buildpack_url} -a {self.app_name}'
         r = call_cmd(cmd)
         return r
 
-    def scale_app_dynos(self, app_name: str, dyno_dict: dict):
+    def scale_app_dynos(self, dyno_dict: dict):
         dynos_data = ' '.join(f'{k}={v}' for k, v in dyno_dict.items())
-        cmd = f'heroku ps:scale -a {app_name} {dynos_data}'
+        cmd = f'heroku ps:scale -a {self.app_name} {dynos_data}'
         r = call_cmd(cmd)
         return r
 
-    def restart_app(self, app_name: str):
-        cmd = f'heroku ps:restart -a {app_name}'
+    def restart_app(self):
+        cmd = f'heroku ps:restart -a {self.app_name}'
         r = call_cmd(cmd)
         return r
